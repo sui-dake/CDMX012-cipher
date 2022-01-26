d18906e453cb9524d99da248cf527282dc78a3b5
sui-dake <sui.vf@tuta.io>
2022-01-26 13:38:15 -0600

    initial commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
 const cipher = {
-  // ...
+    // a = offset num
+    // b = charaASCII
+  encode: function (a, b) {
+    let formula = ((b - 65) + a) % 26 + 65
+    if (formula < 65) {
+     
+      formula = formula + 26
+    }
+    return formula
+  },
+// t = charaASCII
+// s = offset2
+  decode: function (s, t) {
+    let formula = ((t - 65) - s) % 26 + 65
+    if (formula < 65) {
+
+      formula = formula + 26
+    }
+    return formula
+  },
 };
 
 export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,110 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+
+document.getElementById("botonCuadro").onclick = cifra
+
+
+ /////////////
+function cifra (){
+    let i = 0               //contador
+    let cajaTexto = document.getElementById("cuadrotxt").value;      //valor del input
+    let offset = Number(document.getElementById('quantity').value);  // valor del offset
+    let resulta = ''        //mensaje cifrado a mostrar
+
+    while(i < cajaTexto.length){
+        
+        let chara = cajaTexto.slice(i, i +1);
+        let charaASCII = Number(chara.charCodeAt());
+        
+            if (charaASCII != 32){
+           
+            let formula = cipher.encode(offset, charaASCII);
+            let newChara = String.fromCharCode(formula);
+            
+            resulta += newChara
+            i ++
+    
+                
+            } else {
+               
+                let newChara = ' ';
+                    resulta += newChara 
+                    i ++
+    
+                if (i == cajaTexto.length){
+                    document.getElementById('zzb').innerHTML = resulta
+                }
+            }
+    }
+}
+
+///////////////// DESCIFRADO //////////////////////////
+
+document.getElementById("botonCuadro2").onclick = descifra
+
+
+ /////////////
+function descifra (){
+    let j = 0               //contador
+    let cajaTexto = document.getElementById("cuadrotxt2").value;      //valor del input
+    let offset = Number(document.getElementById('quantityTwo').value);  // valor del offset
+    let resulta = ''        //mensaje descifrado a mostrar
+
+    while(j < cajaTexto.length){
+        
+        let chara = cajaTexto.slice(j, j +1);
+        let charaASCII = Number(chara.charCodeAt());
+            if (charaASCII != 32){
+           
+            let formula = cipher.decode(offset, charaASCII);
+            let newChara = String.fromCharCode(formula);
+            
+            resulta += newChara
+            j ++
+    
+                if (j == cajaTexto.length){
+                    document.getElementById('traduc2').innerHTML = resulta
+                }
+            } else {
+               
+                let newChara = ' ';
+                    resulta += newChara 
+                    j ++
+    
+                if (j == cajaTexto.length){
+                    document.getElementById('zzb').innerHTML = resulta
+                }
+            }       
+    }
+}
+
+
+
+
+
+
+//s = string a encriptar, t = valor de offset
+//function caesarC (s, t) {
+  //  const charasM = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    //let ascii = [];
+    //const resuelto = [];
+    //for(let i = 0; i<s.length; i++){
+      //  ascii = charasM.charCodeAt (i);
+                
+        //for(let j =0; j<charasM.length; j++){
+        //if(s[i]=== charasM[j]){
+         //   resuelto.push(String.fromCharCode());
+        //}
+     //}
+    //}
+    //return resuelto.join('');
+ //}
+//cifr ('ABC');
+
+ //function caesar () {
+	//if (codificar = string){
+	//	console.log (b2.value + StringfromCharCode(b2.value$0))}
+//}
+
+
